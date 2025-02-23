673fa9c8fa5e9b0fda10b4109a9728e20a5c86c8
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-02-23 21:33:39 +0530

    finished

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 const container=document.querySelector(".container")
+const btn=document.querySelector("button")
+let size=16;
 
-function creategrid(n){
-    for(let i=0;i<n*n;i++){
+
+
+CreateGrid(size)
+function CreateGrid(size){
+    container.innerHTML=""
+    for(let i=0;i<size*size;i++){
     
     const div=document.createElement("div")
-    div.className="grid-items"
-    div.style.width=`${600/n}px`
-    div.style.height=`${600/n}px`
+    div.classList.add("grid-items");
+    div.style.width=`${600/size}px`
+    div.style.height=`${600/size}px`
     div.style.border = "1px solid #000";
     
     container.appendChild(div)
     }
+    addHoverEffects()
 }
-creategrid(16)
+
+function addHoverEffects(){
 const items=document.querySelectorAll(".grid-items");
 
 items.forEach(item => {
     item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'black';
+        const r=Math.floor(Math.random()*255)
+        const g=Math.floor(Math.random()*255)
+        const b=Math.floor(Math.random()*255)
+        item.style.backgroundColor = `rgb(${[r,g,b].join(",")})`
     });
 });
+}
     
+function getGridSize(){
+    let n=prompt("ENter the size:")
+    size=parseInt(n)
+   
+    CreateGrid(size)
+    
+}
+btn.addEventListener("click",getGridSize)
