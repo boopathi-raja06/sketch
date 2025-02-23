0b5ba6f7fb0d3e65cd7d89e776328c10c16f1dab
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-02-23 18:56:32 +0530

    finished hover effect

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function creategrid(n){
     for(let i=0;i<n*n;i++){
     
     const div=document.createElement("div")
-    div.classList.add="grid-items"
+    div.className="grid-items"
     div.style.width=`${600/n}px`
     div.style.height=`${600/n}px`
     div.style.border = "1px solid #000";
@@ -13,3 +13,11 @@ function creategrid(n){
     }
 }
 creategrid(16)
+const items=document.querySelectorAll(".grid-items");
+
+items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = 'black';
+    });
+});
+    
